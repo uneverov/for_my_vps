a11a06d52741d8f2e3ced543adfcc2638454e6b4
uneverov <uneverov@gmail.com>
2023-03-16 02:49:21 +1000

    test 1
    test 2
    
    test 3

--- flask_vps.py
+++ flask_vps.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def page_1():
+    return render_template('page_1.html')
+
+
+@app.route('/download_gpal')
+def download_gpal():
+    path = "/for_my_vps/gpal.rar"
+    return send_file(path, as_attachment=True)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='194.58.107.248')
+    app.run(debug=True, host='194.58.107.248', port=5000)
